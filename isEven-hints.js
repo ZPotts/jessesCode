510d6ab4115c14ad88b22ab2d926c5b22f1a67a7
ZPotts <zack.potts@gmail.com>
2020-03-28 15:52:19 -0600

    Committing the isEven.js

---
+++ isEven-hints.js
@@ -0,0 +1,25 @@
+
+console.log('##ISEVEN.JS RUNNING##');
+
+//create the output var, using a function
+var output = isEven([1, 4, 5, 6, 10, 12, 13]);
+
+function isEven(originalArray) {
+    //create new empty array
+    var array_of_evens = [];
+    
+    //FOR loop
+    //for (i = 0; every item in the array; increment i )
+        // if the number originalarray is even
+            // then add the number to the new array
+        //end if
+    //end for loop
+
+    //return new array
+    return array_of_evens;
+}
+
+//log the output var, once it's been created
+console.log(output);
+
+//
